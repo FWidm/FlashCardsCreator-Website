bb8e5848015bbbc3c1764f826924870c46dcccdb
FWidm <fabian.widmann@uni-ulm.de>
2017-02-21 16:49:40 +0100

    Implemented redirect to login if useremail cookie is not set.

--- js/main.js
+++ js/main.js
@@ -4,28 +4,46 @@ var port = "9000";
 //disable cache for REST calls globally
 $.ajaxSetup({ cache: false });
 
-$(function() {
-    $("#sign-in").click( function()
-        {
-            getHeartbeat();
+//redirect every time if no cookie useremail exists.
+$( document ).ready(function() {
+    console.log( "ready! loc="+window.location.pathname );
+    var cookie=readCookie("useremail");
+    if(!cookie && window.location.pathname != '/FlashCardsCreator/login.html'){
+        console.log("Redirect to login!");
+        window.location.href("login.html");
+    }
+});
 
-            var email=$("#email").val();
-            var password=$("#password").val();
-            console.log("email="+email);
-            console.log("pw="+password);
+$(function() {
+    $("#buttonLogin").click( function()
+        {
+            var email=$("#inputEmail").val();
+            var password=$("#inputPassword").val();
+
             if(email.length<=0||password.length<=0)
                 console.log("Password or email is not set, please try again")
             else {
-                //retrieve token
+                if(checkCredentials(email,password)){
+                    var days=10;
+                    createCookie("useremail",email,days);
+                    //create cookie for token
+                    window.location.href("index.html");
+                }
+
             }
         }
     );
 });
 
+function checkCredentials(email, password) {
+    //ajax request
+    return true;
+}
+
 function getHeartbeat() {
     jQuery.ajax({
         type: "GET",
-        url: "http://localhost:9000/heartbeat",
+        url: "http://"+ip+":"+port+"/heartbeat",
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         success: function (data, status, jqXHR) {
@@ -36,4 +54,47 @@ function getHeartbeat() {
             console.log("status="+status);
         }
     });
+}
+
+function get(resource) {
+    jQuery.ajax({
+        type: "GET",
+        url: "http://"+ip+":"+port+"/"+resource,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (data, status, jqXHR) {
+            console.log("status="+status);
+            console.log("data="+data.currentDate);
+            return [status, data];
+        },
+        error: function (jqXHR, status) {
+            console.log("status="+status);
+            return status
+        }
+    });
+}
+
+function createCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    createCookie(name,"",-1);
 }
