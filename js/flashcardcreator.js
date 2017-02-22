662507053cb205cb8bca3bd9f9e9a3ebdf25a22c
FWidm <fabian.widmann@uni-ulm.de>
2017-02-22 21:09:11 +0100

    fixed bugs, finished form

--- js/flashcardcreator.js
+++ js/flashcardcreator.js
@@ -4,7 +4,6 @@
 var countAnswers = 1;
 
 //Add a new Answer
-
 $("#addAnswerTemplate").click(function () {
         console.log("Woop");
         countAnswers++;
@@ -17,27 +16,31 @@ $("#addAnswerTemplate").click(function () {
             + '<label for="answer-' + countAnswers + '-url">Answer URL:</label>'
             + '<input class="form-control" rows="5" id="answer-' + countAnswers + '-url"></input>'
             + '<label>Is the answer correct? (for Multiple Choice cards!)</label><br/>'
-            + '<label class="radio-inline"><input type="radio" name="answer-' + countAnswers + '-optCorrect">Correct</label>'
-            + '<label class="radio-inline"><input type="radio" name="answer-' + countAnswers + '-optFalse">False</label>'
+            + '<label class="radio-inline"><input type="radio" name="answer-' + countAnswers + '-correct">Correct</label>'
+            + '<label class="radio-inline"><input type="radio" name="answer-' + countAnswers + '-correct">False</label>'
             + '<br/>'
             /*+ '<button type="button" class="btn btn-default pull-right" id="deleteAnswerTemplate"> <span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>'*/
             + '<hr></div>');
-        if($("#deleteAnswerTemplate").hasClass("disabled")){
-            $("#deleteAnswerTemplate").addClass("active");
-            $("#deleteAnswerTemplate").removeClass("disabled");
-        }
+        setDeleteButtonActive(true);
     }
 );
 
 $("#deleteAnswerTemplate").click(function () {
-    if(countAnswers>1){
-        $("#answer-" + countAnswers).remove();
+        if (countAnswers > 1) {
+            $("#answer-" + countAnswers).remove();
 
-        countAnswers--;
+            countAnswers--;
 
-    }
-    if(countAnswers<=1)
-        $("#deleteAnswerTemplate").addClass("disabled");
-        $("#deleteAnswerTemplate").removeClass("active");
+        }
+        if (countAnswers <= 1)
+            setDeleteButtonActive(false);
     }
 );
+
+function setDeleteButtonActive(setActive) {
+    var button=$("#deleteAnswerTemplate");
+    if(setActive)
+        button.removeClass("disabled").addClass("active");
+    else
+        button.removeClass("active").addClass("disabled");
+}
